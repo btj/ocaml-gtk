35ca39fe21e71a10018951f2a0c65b71008833ed
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-01-04 13:37:30 +0100

    example0_oo.ml: fix a few code format issues

--- example0_oo.ml
+++ example0_oo.ml
@@ -8,9 +8,9 @@ let () =
 
     let pack = Gtk.box Gtk.Orientation.horizontal 0 in
     let button = Gtk.Button.new_with_label "Click Me!" in
-    pack#append (button :> Gtk.widget) ;
+    pack#append (button :> Gtk.widget);
     window#set_child (pack :> Gtk.widget);
-    ignore @@ button#signal_connect_clicked (fun () -> print_endline("I'm clicked"));
+    ignore @@ button#signal_connect_clicked (fun () -> print_endline "I'm clicked");
   end;
   let status = app#run Sys.argv in
   exit status
